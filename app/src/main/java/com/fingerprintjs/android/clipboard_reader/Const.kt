86a3d652ae0294577587cf6eac6cfa68f245765a
Alexey Verkhovsky <alexey.verkhovsky@mail.ru>
2023-10-12 12:32:24 +0300

    Remove unused resources

--- app/src/main/java/com/fingerprintjs/android/clipboard_reader/Const.kt
+++ app/src/main/java/com/fingerprintjs/android/clipboard_reader/Const.kt
@@ -2,4 +2,4 @@ package com.fingerprintjs.android.clipboard_reader
 
 
 const val GITHUB_PAGE_URL = "https://github.com/fingerprintjs/blog-android-clipboard-security-demo"
-const val ARTICLE_PAGE_URL = "https://fingerprintjs.com/blog/android-14-clipboard-manager-security-issue/"
+const val ARTICLE_PAGE_URL = "https://fingerprint.com/blog/android-14-clipboard-security-leak/"
--- app/src/main/java/com/fingerprintjs/android/clipboard_reader/WelcomeActivity.kt
+++ app/src/main/java/com/fingerprintjs/android/clipboard_reader/WelcomeActivity.kt
@@ -3,13 +3,10 @@ package com.fingerprintjs.android.clipboard_reader
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.UnderlineSpan
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.fingerprintjs.android.clipboard_reader.clipboard_history_screen.ClipboardHistoryActivity
@@ -23,22 +20,12 @@ class WelcomeActivity : AppCompatActivity() {
 
         val startButton = findViewById<View>(R.id.get_started_button)
         startButton.setOnClickListener {
-            if (!Settings.canDrawOverlays(this)) {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R && !Settings.canDrawOverlays(this)) {
                 showRequestPermissionDialog {
                     openScreenWithRequestPermissionIfNeeded()
                 }
-            }
-            else openScreenWithRequestPermissionIfNeeded()
+            } else openScreenWithRequestPermissionIfNeeded()
         }
-
-        val termsButton = findViewById<TextView>(R.id.terms_btn)
-
-        val spannableTermsBtnText = SpannableString(this.getString(R.string.agreement))
-        spannableTermsBtnText.setSpan(UnderlineSpan(), 39, 44, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        termsButton.text = spannableTermsBtnText
-        termsButton.setOnClickListener { openScreen(TermsActivity::class.java) }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,8 +44,7 @@ class WelcomeActivity : AppCompatActivity() {
             .Builder(this)
             .setTitle("Permission is required")
             .setMessage("Draw over other apps permission is required to launch the demo")
-            .setPositiveButton("Got it")  {
-                    dialog, which ->
+            .setPositiveButton("Got it") { dialog, _ ->
                 dialog.dismiss()
                 listener.invoke()
             }
@@ -66,7 +52,7 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun openScreenWithRequestPermissionIfNeeded() {
-        if (!Settings.canDrawOverlays(this)) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R && !Settings.canDrawOverlays(this)) {
             val intent = Intent(
                 Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:$packageName")
